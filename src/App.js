a859600467520f2483482c655d190a53bdb174b0
Yogita Sinkar <yogita.sinkar5@gmail.com>
2023-06-29 18:39:09 +0000

    Add auth frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withAuthenticator} from '@aws-amplify/ui-react';
 import './App.css';
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import KYCContainer from './components/KYCContainer'
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World !
-      </header>
+    <div>
+        <KYCContainer />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/components/KYCContainer.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+
+import { AmplifySignOut } from '@aws-amplify/ui-react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+import Button from 'react-bootstrap/Button'
+
+
+export default () => {
+
+  const [currentTabKey, setCurrentTabKey] = useState("welcome");
+  
+  const startKyc = () => {
+    setCurrentTabKey("Liveliness");
+
+  }
+
+  const onSelectTab = (eventkey) => {
+    console.log("printing event key ",eventkey);
+    setCurrentTabKey(eventkey);
+  }
+
+  return (
+   <div>
+  <Container>
+  <Row>
+    <Col>
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#"><h2 className="app-title">Video KYC</h2></Navbar.Brand>
+      <span className="logout">
+      <AmplifySignOut/>
+      </span>
+    </Navbar>
+    </Col>
+  </Row>
+  <Row><Col><br></br></Col></Row>
+  <Row>
+    <Col>
+    <Tabs defaultActiveKey={currentTabKey} activeKey = {currentTabKey} id="uncontrolled-tab-example" onSelect={onSelectTab}>
+        <Tab eventKey="welcome" title="Welcome">
+                <h2 className="tab-element-align">Welcome to video KYC</h2>
+                <div className="tab-element-align">
+                    <p>The KYC process consists of 3 simple steps. </p>
+                    <ul>
+                        <li>Liveliness Detection - The user will do a series of face gestures to determine whether its a live feed</li>
+                        <li>Upload Documents - upload valid ID documents to use for verification.</li>
+                        <li>Validation and summary</li>
+                    </ul>
+                </div>
+                <p className="tab-button-align">
+                    <Button variant="primary" onClick = {startKyc}>Start</Button>
+                </p>
+        </Tab>
+        <Tab eventKey="Liveliness" title="Liveliness Test">
+            <div>
+                Liveliness Check !
+            </div>
+        </Tab>
+        <Tab eventKey="UploadDocs" title="Upload Documents">
+            <div>
+            Upload Documents
+            </div>
+        </Tab>
+        <Tab eventKey="AnalysisDetails" title="Details of Analysis">
+            Summary Details
+        </Tab>
+        </Tabs>
+    </Col>
+  </Row>
+  </Container>
+  </div>
+   
+   
+
+  )
+}
